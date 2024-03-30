f0e6a7290433f183d702c9e91e5002fcf26ab09a
DoubtCrack <1.edujock@gmail.com>
2024-03-30 10:20:07 +0530

    feat: router-dom setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { Dashboard } from "./components/header/header";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+  {
+    path: "dev",
+    element: <div>Hellcsdo dev!</div>,
+  },
+]);
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;

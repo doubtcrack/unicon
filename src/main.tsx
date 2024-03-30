e42591ec15b811a48de8ff1746ade7f61aa7a87a
DoubtCrack <1.edujock@gmail.com>
2024-03-30 11:08:07 +0530

    feat: introduced redux with RTK

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./index.css";
+import { Provider } from "react-redux";
+import Store from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="theme">
-      <App />
+      <Provider store={Store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );

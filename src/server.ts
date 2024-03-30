339c9c1876d83d7d9e93d46ce74eed04c318969b
DoubtCrack <1.edujock@gmail.com>
2024-03-30 11:02:50 +0530

    feat: made connection with server

---
+++ src/server.ts
@@ -0,0 +1,5 @@
+const url = import.meta.env.VITE_APP_BACKEND_URL;
+
+export const server = `${url}/api/v2`;
+export const backend_url = `${url}`;
+export const socket_server = import.meta.env.VITE_APP_SOCKET_URL;
